56b55038730c6882e1daeeec221ea1773292140e
Andy Malak <amalak@albany.edu>
2025-11-12 16:25:31 +0000

    added logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 
+import morgan from "morgan";
+
 const app = express();
+app.use(morgan('dev'));
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
